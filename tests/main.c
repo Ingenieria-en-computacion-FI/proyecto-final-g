2a4803094afeb6c844df6050e354251c510f0a7a
Abril Ugalde <abril.ugaldevelasc@gmail.com>
2026-06-03 14:05:13 -0600

    Modifique main y tests

--- tests/main.c
+++ tests/main.c
@@ -1,5 +1,16 @@
 #include <stdio.h>
 
+void test_stack_push_pop();
+void test_stack_empty();
+
+void test_queue_fifo();
+
+void test_round_robin_behavior();
+
+void test_fifo_scheduler();
+
+void test_first_fit();
+
 int main() {
     printf("Running integration tests...\n");
 
@@ -13,10 +24,6 @@ int main() {
     test_fifo_scheduler();
 
     test_first_fit();
-
-    //test_bruteforce();
-    //test_greedy();
-    //test_dp();
 
     printf("All tests passed!\n");
 
